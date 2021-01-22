79057babdd6cd297ad010b086c946f6d18676931
Igor Gnatiuk <igor.gnatiuk111@gmail.com>
2021-01-22 14:46:14 +0200

    footer

--- js/modal.js
+++ js/modal.js
@@ -11,4 +11,10 @@
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
-  })();
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+      menuBtnRef.classList.toggle("is-open");
+      mobileMenuRef.classList.toggle("is-open");});
+    })();
+  
