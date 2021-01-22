ec6cc0690cd718c675870f236c572347e50b951e
Igor Gnatiuk <igor.gnatiuk111@gmail.com>
2021-01-22 21:25:27 +0200

    add header

--- js/modal.js
+++ js/modal.js
@@ -7,14 +7,16 @@
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
+
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+
     menuBtnRef.addEventListener("click", () => {
-      menuBtnRef.classList.toggle("is-open");
-      mobileMenuRef.classList.toggle("is-open");});
+    menuBtnRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");});
     })();
-  
+
+    
